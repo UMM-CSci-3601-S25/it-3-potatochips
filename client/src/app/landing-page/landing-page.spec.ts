a2bef54a83a93fae9a8e492c79620d6fdd875b5d
Dylan Packer <packe089@morris.umn.edu>
2025-03-24 11:34:28 -0500

    All test now pass!

--- client/src/app/landing-page/landing-page.spec.ts
+++ client/src/app/landing-page/landing-page.spec.ts
@@ -5,23 +5,25 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 import { HomeComponent } from './landing-page';
 import { By } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         BrowserAnimationsModule,
-        RouterModule.forRoot([]),
+        RouterTestingModule,
         MatToolbarModule,
         MatIconModule,
         MatSidenavModule,
         MatCardModule,
         MatListModule,
         HttpClientModule,
+        FormsModule,
         HomeComponent
       ],
     }).compileComponents();
@@ -33,11 +35,19 @@ describe('HomeComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have a button to join a game', () => {
+  it('should call createGame when Create New Game button is clicked', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.componentInstance;
+    spyOn(component, 'createGame');
+    const createButton = fixture.debugElement.query(By.css('button#create-game'));
+    createButton.triggerEventHandler('click', null);
+    expect(component.createGame).toHaveBeenCalled();
+  });
+
+  it('should have a Join Game button', () => {
     const fixture = TestBed.createComponent(HomeComponent);
     fixture.detectChanges();
-    const joinButton = fixture.debugElement.query(By.css('routerLink="/game/{{joinId}}'));
+    const joinButton = fixture.debugElement.query(By.css('button#join-game'));
     expect(joinButton).toBeTruthy();
-    expect(joinButton.nativeElement.textContent).toContain('Join Game');
   });
 });

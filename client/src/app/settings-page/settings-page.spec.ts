769f4964ab3328407a5d97c0617ce6a00646ffdb
Dylan Packer <packe089@morris.umn.edu>
2025-04-04 12:30:28 -0500

    added test for settings page it is now at 78.26 global coverage

--- client/src/app/settings-page/settings-page.spec.ts
+++ client/src/app/settings-page/settings-page.spec.ts
@@ -1,27 +1,71 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { TestBed, waitForAsync, ComponentFixture } from '@angular/core/testing'; // Import ComponentFixture
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule } from '@angular/common/http'; // Add this import
 import { SettingsComponent } from './settings-page';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Game } from '../game'; // Import the Game interface
+import { signal } from '@angular/core'; // Import signal from Angular
 
 describe('SettingsComponent', () => {
+  let component: SettingsComponent;
+  let fixture: ComponentFixture<SettingsComponent>; // Explicitly type fixture
+  let httpMock: HttpTestingController;
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         BrowserAnimationsModule,
         MatCardModule,
         RouterTestingModule,
-        HttpClientModule, // Add this import
+        HttpClientTestingModule,
         SettingsComponent
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of({ get: () => '12345' }) // Mock route parameter
+          }
+        }
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
-  it('should create', () => {
-    const fixture = TestBed.createComponent(SettingsComponent);
-    const component = fixture.componentInstance;
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SettingsComponent);
+    component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should send a PUT request with the correct payload when updateGameSettings is called', () => {
+    // Mock the GET request for the game signal
+    const mockGame: Game = { _id: '12345' } as Game;
+    const getReq = httpMock.expectOne('/api/game/12345');
+    expect(getReq.request.method).toBe('GET');
+    getReq.flush(mockGame);
+
+    // Mock the game signal to return a valid game object
+    component.game = signal<Game | undefined>(mockGame);
+
+    // Set the judgeOption signal value
+    component.judgeOption.set(true);
+
+    // Call the method
+    component.updateGameSettings();
+
+    // Expect an HTTP PUT request
+    const putReq = httpMock.expectOne('/api/game/edit/12345');
+    expect(putReq.request.method).toBe('PUT');
+    expect(putReq.request.body).toEqual({ $set: { winnerBecomesJudge: true } });
+
+    // Respond to the PUT request to complete it
+    putReq.flush({});
   });
 });

aabfed6adad295c3b3999dc6f4f34a3a4bf39622
K.K. Lamberty <lamberty@morris.umn.edu>
2025-04-22 18:14:26 -0500

    Turn off eslint rule
    Folks on this project should just be careful, then, about their use of === versus ==.

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -62,9 +62,6 @@ export default [{
 })), {
     files: ["**/*.html"],
     rules: {
-      "@angular-eslint/template/eqeqeq": ["warn", {
-          allowNullOrUndefined: true,
-        }
-      ],
+      "@angular-eslint/template/eqeqeq": "off",
     },
 }];

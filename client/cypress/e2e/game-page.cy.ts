a7d7db19a52329113b308c4d7de50dd07d29a7d4
Mahathir <mosta044@umn.edu>
2025-04-25 12:02:57 -0500

    Added tests (e2e) but they fail
    Failing test | Coming back to it later. (Commented out the changes)

--- client/cypress/e2e/game-page.cy.ts
+++ client/cypress/e2e/game-page.cy.ts
@@ -26,11 +26,16 @@ describe('Game page', () => {
     page.nameButton().click();
   });
 
-  it('Should read response input', () => {
-    //page.readResponseInput('GEJ');
-    //page.responseButton().click();
+  it('Should enter response input', () => {
+    page.readNameInput('GEJ');
+    page.nameButton().click();
+    page.readResponseInput('GEJ');
+    page.responseButton().click();
+    cy.get('.response').contains('GEJ').should('exist');
   });
 
-
-
+  it('Should shuffleArray', () => {
+    const array = [1, 2, 3, 4, 5];
+    const shuffledArray = page.shuffleArray(array);
+  });
 });

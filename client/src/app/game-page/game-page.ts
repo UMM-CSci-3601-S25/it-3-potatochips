70866611bbbf5ae39d00e9092e5e0d38335586a8
Mackenzie Amoako <amoak017@umn.edu>
2025-04-29 14:30:39 -0500

    added a snackbar

--- client/src/app/game-page/game-page.ts
+++ client/src/app/game-page/game-page.ts
@@ -14,6 +14,7 @@ import { of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
 //import { console } from 'inspector';
 
 
